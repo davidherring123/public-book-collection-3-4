107d496d6857700e50dab9c2166d32304725c0c2
davidherring123 <david.herring8680@gmail.com>
2025-02-03 21:39:11 -0600

    attempt to get heroku to work

--- app/views/books/_book.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! book, :id, :title, :author, :price, :date_published, :created_at, :updated_at
-json.url book_url(book, format: :json)
--- db/migrate/20250126231541_add_attributes_to_books.rb
+++
@@ -1,7 +0,0 @@
-class AddAttributesToBooks < ActiveRecord::Migration[6.1]
-  def change
-    add_column :books, :author, :string
-    add_column :books, :price, :float
-    add_column :books, :date_published, :date
-  end
-end
